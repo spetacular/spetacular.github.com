c97b2d57148cd12d5ff0813984b13a8fcc4e7a5c
yanxiaokun <spetacular@users.noreply.github.com>
2023-10-27 22:34:28 +0800

    增加public

---
+++ public/deploy-0395/deploy.sh
@@ -0,0 +1,6 @@
+hexo clean
+hexo generate
+cp -r public/* /Users/yanxiaokun/Documents/GitHub/spetacular.github.com/
+cp ./_config.yml /Users/yanxiaokun/Documents/GitHub/spetacular.github.com/deploy-0395/
+cp ./node_modules/hexo-theme-amazing/_config.yml /Users/yanxiaokun/Documents/GitHub/spetacular.github.com/deploy-0395/
+cp ./deploy.sh /Users/yanxiaokun/Documents/GitHub/spetacular.github.com/deploy-0395/
